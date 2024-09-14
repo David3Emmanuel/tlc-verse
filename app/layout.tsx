60304eb1d7c4e38bd873ca7e5f3a8569fd4b6e50
David Emmanuel <david3emmanuel@gmail.com>
2024-09-14 19:05:56 +0100

    added seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import '@/app/globals.css'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  verification: {
+    google: 'grIhPa2bbbnT1kt3eE6tucCV3uuEVcmSurWwrDg02AE',
+  },
+}
 
 export default function RootLayout({ children }: {
   children: React.ReactNode
